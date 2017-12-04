e27949a08414f0996c8df5b03e4b95eef08390ad
rossr87 <singularity.imminent@gmail.com>
2017-12-04 15:46:04 +0000

    Remove Two Command Class definitions to demonstrate functional object manipulation with lambda expressions

--- src/jcommand_demo/Launderette.java
+++ src/jcommand_demo/Launderette.java
@@ -10,10 +10,13 @@ public class Launderette {
 		/*
 		 * The command needs a reference to the receiver
 		 */
-		Wash40DegreesCommand wash40 = new Wash40DegreesCommand(washingMachine);
+		Wash60DegreesCommand wash60 = new Wash60DegreesCommand(washingMachine);
 		Spin600RpmCommand spin600 = new Spin600RpmCommand(washingMachine);
 		
-		controlPanel.setCommand(wash40, spin600);
+		/*
+		 * Command objects are passed to the Invoker.
+		 */
+		controlPanel.setCommand(wash60, spin600);
 		controlPanel.startButtonWasPushed();
 		
 		washingMachine.getTemperature();
@@ -22,27 +25,92 @@ public class Launderette {
 		SimpleWasherReceiver washingMachine2 = new SimpleWasherReceiver();
 
 		/*
-		 * At the moment, we can only set a command's reference to the receiver it
-		 * is commanding at instantiation time (this is easily changed).
+		 * Ok, so now we will not create these objects.
+		 * The following commented out objects,
+		 * These following ConcreteObjects implement a Command interface and are
+		 * can be considered "functional objects" as they implement one method.
+		 * 
+		 * The instantiations of these, contain one method definition.
 		 */
-		Wash40DegreesCommand wash40_2 = new Wash40DegreesCommand(washingMachine2);
-		Spin800RpmCommand spin800 = new Spin800RpmCommand(washingMachine2);
+		//Wash40DegreesCommand wash40_2 = new Wash40DegreesCommand(washingMachine2);
+		//Spin800RpmCommand spin800 = new Spin800RpmCommand(washingMachine2);
 		
-		controlPanel.setCommand(wash40_2, spin800);
+		/*
+		 * Time to pass the command objects to the Invoker.
+		 * 
+		 * Note that:
+		 * Instead of us having to write a class definition, then implement a method A, which
+		 * calls another method, B. We simply use a lambda expression which calls method B.
+		 */
+		//controlPanel.setCommand(wash40_2, spin800);
+		controlPanel.setCommand(() -> { washingMachine2.setWashTemperature(WashTemperature.FOURTY); },
+								() -> { washingMachine2.setSpinRPM(SpinRPM.FOURTEEN_HUNDRED); });
 		controlPanel.startButtonWasPushed();
 		
 		washingMachine2.getTemperature();
 		washingMachine2.getSpinRpm();
 		
+		/*
+		 * We create a new Receiver object
+		 */
 		SimpleWasherReceiver washingMachine3 = new SimpleWasherReceiver();
-		Wash90DegreesCommand wash90 = new Wash90DegreesCommand(washingMachine3);
-		Spin1400RpmCommand spin1400 = new Spin1400RpmCommand(washingMachine3);
+
+		/*
+		 * Now, instead of creating two new commands objects...
+		 * (These are "functional objects" - the object's implement one function
+		 * after implementing an interface...)
+		 */
+		//Wash90DegreesCommand wash90 = new Wash90DegreesCommand(washingMachine3);
+		//Spin1400RpmCommand spin1400 = new Spin1400RpmCommand(washingMachine3);
 		
-		controlPanel.setCommand(wash90, spin1400);
+		/*
+		 * We have no (functional) object references to pass to the setCommand method.
+		 * Instead we pass lamba expressions....
+		 * The method definition expects:
+		 *  Command interface which is execute(), and that is all. so the name is not really
+		 *  import, but all that is important is:
+		 *  	- return type: void
+		 *  	- arguments: none
+		 *  So it's easy to match against this method.
+		 *  Now, what does this method do? We just pass in the body using the lambda expression...
+		 *  
+		 *  Remember, the lambda expression we pass to the member function here is the body of 
+		 *  what the functional object's contains.
+		 */
+		controlPanel.setCommand(() -> { washingMachine3.setWashTemperature(WashTemperature.NINETY); },
+								() -> { washingMachine3.setSpinRPM(SpinRPM.FOURTEEN_HUNDRED); });
 		controlPanel.startButtonWasPushed();
 		
 		washingMachine3.getTemperature();
 		washingMachine3.getSpinRpm();
+		
+		
+		/*
+		 * Create a new Receiver.
+		 */
+		SimpleWasherReceiver washingMachine4 = new SimpleWasherReceiver();
+		
+		/*
+		 * No need to create new command objects!
+		 * But what we WOULD place in the definition of their object's gets passed in to
+		 * the appropritate method by the lambda function.
+		 */
+		controlPanel.setCommand( () -> { washingMachine4.setWashTemperature(WashTemperature.FOURTY); },
+								 () -> { washingMachine4.setSpinRPM(SpinRPM.EIGHT_HUNDRED);});
+		
+		/*
+		 * Ah, ha! 
+		 * We have removed the class definitions for the below Command objects!
+		 * Instead of Command object's implementing the interface and defining a single method, that calls
+		 * code in the receiver.
+		 * With the Invoker, still set to receive object's passed by the lambda function
+		 * (functional objects), that conform to the interface type, we just pass in to code
+		 * that the command object's method would call (upon the the receiver).
+		 */
+		
+		controlPanel.startButtonWasPushed();
+		washingMachine4.getTemperature();
+		washingMachine4.getSpinRpm();
 	}
 
 }
